e95827dae4ee3b55881ea7e523d79531c27f9d4a
cgranja <cgranja@student.42.fr>
2023-02-04 17:43:10 +0100

    Joincommand

---
+++ Objs/commandhandler.d
@@ -0,0 +1,5 @@
+Objs/commandhandler.o: Srcs/commandhandler.cpp \
+ Srcs/../Includes/commandhandler.hpp Srcs/../Includes/colors.hpp \
+ Srcs/../Includes/command_list.hpp Srcs/../Includes/server.hpp \
+ Srcs/../Includes/client.hpp Srcs/../Includes/channel.hpp \
+ Srcs/../Includes/utils.hpp Srcs/../Includes/commandhandler.hpp
---
+++ Objs/server.d
@@ -0,0 +1,5 @@
+Objs/server.o: Srcs/server.cpp Srcs/../Includes/server.hpp \
+ Srcs/../Includes/colors.hpp Srcs/../Includes/client.hpp \
+ Srcs/../Includes/channel.hpp Srcs/../Includes/server.hpp \
+ Srcs/../Includes/utils.hpp Srcs/../Includes/commandhandler.hpp \
+ Srcs/../Includes/command_list.hpp
