34e5e84db1802497e084ca4733de0a5d001bc8b0
WindEole <derighett.isabelle@wanadoo.fr>
2023-02-13 18:38:24 +0100

    merge quit/who/oper

--- Includes/command_list.hpp
+++ Includes/command_list.hpp
@@ -146,19 +146,20 @@ class ModeCommand : public Command
 		void mode_channel(Client *client, Channel *channel, std::vector<std::string> arg);
 };
 
-<<<<<<< HEAD
 class QuitCommand : public Command
 {
 	public:
 		QuitCommand(Server *server);
 		~QuitCommand();
-=======
+	
+		void execute(Client *client, std::vector<std::string> arg);
+};
+
 class OperCommand : public Command
 {
 	public:
 		OperCommand(Server *server);
 		~OperCommand();
->>>>>>> 992e1cf151d1b89fab7d267a6b1ee134225eee80
 
 		void execute(Client *client, std::vector<std::string> arg);
 };
